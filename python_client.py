36bd2ebff6fc569bf95fd87848ecc35489f00e1a
j-pats <148277258+j-pats@users.noreply.github.com>
2024-09-05 17:36:12 -0600

    Update python_client.py
    Updates to python script to be clearer and remove unnecessary comments and values.

--- python_client.py
+++ python_client.py
@@ -26,34 +26,31 @@ args = parser.parse_args()
 client = udp_client.SimpleUDPClient(args.ip, args.port)
 
 # Save original analog integer values
-oldAnalogValue0 = channel0.value
-oldAnalogValue1 = channel1.value
+oldValue0 = channel0.value
+oldValue1 = channel1.value
 
-# send inital values
+# Send inital values
+# Converted digital value: channelX.value
+# Analog voltage: channelX.voltage
 client.send_message("/analog0", channel0.value)
 client.send_message("/analog1", channel1.value)
 
-# Loop to read analog inputs
 while True:
-    #print("Analog Value 1: ", channel1.value, "Voltage 1: ", channel1.voltage)
-
-    #Get analog values from ADS1115
-    analogValue0 = channel0.value
+    #Read converted values from ADS1115
+    value0 = channel0.value
     analogValue1 = channel1.value
 
-    #If analog value changed send as OSC message
-    if abs(oldAnalogValue0 - analogValue0) > 3:
+    #If value changed send as OSC message
+    if abs(oldValue0 - value0) > 3:
         #Send new value A0
-        client.send_message("/analog0", analogValue0)
-        oldAnalogValue0 = analogValue0
-        print("Analog Value 0: ", analogValue0)
+        client.send_message("/analog0", value0)
+        oldValue0 = value0
+        print("Analog Value 0: ", value0)
 
-
-    if abs(oldAnalogValue1 - analogValue1) > 3:
+    if abs(oldValue1 - analogValue1) > 3:
         #Send new value A1
-        client.send_message("/analog1", analogValue1)
-        oldAnalogValue1 = analogValue1
-        print("Analog Value 1: ", analogValue1)
-
-    # Delay sampling time (s)
+        client.send_messageanalog1", analogValue1)
+        oldValue1 = analogValue1        print("Analog Value 1: ", )
+    
+    # Delay sampling time (~30 times second)
     time.sleep(0.032)
